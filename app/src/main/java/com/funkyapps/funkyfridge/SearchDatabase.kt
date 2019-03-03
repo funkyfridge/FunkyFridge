e775e7192c5e480fe8faa3a71f0e24c5996a221a
Saurav Ghosal <sghosal9@gatech.edu>
2019-03-02 23:28:41 -0500

    updated seach database to return a json object instead of string

--- app/src/main/java/com/funkyapps/funkyfridge/SearchDatabase.kt
+++ app/src/main/java/com/funkyapps/funkyfridge/SearchDatabase.kt
@@ -1,42 +1,18 @@
 package com.funkyapps.funkyfridge
 
-import android.util.Log
-import java.io.*
-import java.net.*
+import org.json.JSONException
+import org.json.JSONObject
 
-class SearchDataBase {
+class Search {
+    protected lateinit var json: JSONObject
+
     init {
         try {
-            // should print out all the nutrition info of the item
-            getHTML("https://api.nutritionix.com/v1_1/item?upc=[UPC]&appId=359b6d3a&appKey=0f9a03a179334c56721fcf70cc8600b9") //insert upc into [UPC] to search for item
-        } catch (e: Exception) {
+            json =
+                JSONObject("https://api.nutritionix.com/v1_1/item?upc=[UPC]&appId=359b6d3a&appKey=0f9a03a179334c56721fcf70cc8600b9")
+        } catch (e: JSONException) {
             e.printStackTrace()
         }
 
-    }
-
-    companion object {
-        @Throws(Exception::class)
-        //Should return a string with the product's information
-        fun getHTML(urlToRead: String): String {
-            val result = StringBuilder()
-            val url = URL(urlToRead)
-            val conn = url.openConnection() as HttpURLConnection
-            conn.requestMethod = "GET"
-            val rd = BufferedReader(InputStreamReader(conn.inputStream))
-            var line: String? = null;
-            while ({ line = rd.readLine(); line }() != null) {
-                result.append(line)
-            }
-            rd.close()
-            return result.toString()
-        }
-
-        @Throws(Exception::class)
-        @JvmStatic
-        fun main(args: Array<String>) {
-            println(getHTML(args[0])) //prints out string containing all nutrition info
-
-        }
     }
 }
